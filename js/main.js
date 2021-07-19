e09fa127ebe61a72a8f393a1890f58202e73c591
spata00 <sararares12@gmail.com>
2021-07-19 12:51:55 +0200

    stampiamo in una lista

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
 var numeri = [];
+var output="";
 var numeri_intersazioni=100;
 for (var i=0; i<numeri_intersazioni;i++){
     numeri.push(i+1);
     if(numeri[i]%3==0 || numeri[i]%5==0){
         if(numeri[i]%3==0 && numeri[i]%5==0){
-            console.log("FizzBuzz")
+            console.log("FizzBuzz");
+            output+="<li> fizz</li>";
         } else if(numeri[i]%3==0){
-            console.log("Fizz")
+            console.log("Fizz");
+            output+="<li> buzz</li>";
         } else{
-            console.log("Buzz")
+            console.log("Buzz");
+            output+="<li> fizzbuzz</li>";
         }
     }
     else {
-        console.log(numeri[i])
+        output+="<li>"+numeri[i]+"</li>";
     }
 }
+document.getElementById("lista").innerHTML=output;
