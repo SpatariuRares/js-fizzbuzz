0f8e1880425b31293c228e1c8fb28bb5f8a28d51
spata00 <sararares12@gmail.com>
2021-07-19 12:59:37 +0200

    bonus

---
+++ bonus/js/main.js
@@ -0,0 +1,16 @@
+var numeri = [];
+var output="";
+var numeri_intersazioni=100;
+for (var i=0; i<numeri_intersazioni;i++){
+    numeri.push(i+1);
+    if(numeri[i]%3==0 && numeri[i]%5==0){
+        output+="<li> fizz</li>";
+    } else if(numeri[i]%3==0){
+        output+="<li> buzz</li>";
+    } else if (numeri[i]%5==0){
+        output+="<li> fizzbuzz</li>";
+    }else {
+        output+="<li>"+numeri[i]+"</li>";
+    }
+}
+document.getElementById("lista").innerHTML=output;
